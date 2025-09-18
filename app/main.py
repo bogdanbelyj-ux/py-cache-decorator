df731028028ef72319ee48de9dd0a4fe263948dd
Bohdan <bogdanbelyj@gmail.com>
2025-09-17 22:13:06 -0600

    exercise was fixed

--- app/main.py
+++ app/main.py
@@ -1,17 +1,19 @@
 from typing import Callable
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     result = {}
+    @wraps(func)
 
     def wrapper(*args, **kwargs) -> Callable:
         key = (*args, tuple(sorted(kwargs.items())))
         if key not in result:
-            result[key] = func(*args, ** kwargs)
+            result[key] = func(*args, **kwargs)
             print("Calculating new result")
+            return result[key]
         else:
             print("Getting from cache")
-        return result[key]
     return wrapper
 
 
@@ -27,7 +29,7 @@ def long_time_func_2(n_tuple: tuple, power: int) -> int:
 
 long_time_func(1, 2, 3)
 long_time_func(2, 2, 3)
-long_time_func_2((5, 6, 7), 5)
 long_time_func(1, 2, 3)
+long_time_func_2((5, 6, 7), 5)
 long_time_func_2((5, 6, 7), 10)
 long_time_func_2((5, 6, 7), 10)
